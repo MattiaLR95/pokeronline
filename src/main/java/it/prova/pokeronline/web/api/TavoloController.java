55913ceb5bb44292639392bb3ce043fc1be69dd0
MattiaLR95 <laroccamattia@virgilio.it>
2022-12-02 16:02:45 +0100

    Implementata la funzione di rimozione tavolo

--- src/main/java/it/prova/pokeronline/web/api/TavoloController.java
+++ src/main/java/it/prova/pokeronline/web/api/TavoloController.java
@@ -6,11 +6,14 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import it.prova.pokeronline.dto.TavoloDTO;
@@ -50,5 +53,11 @@ public class TavoloController {
 
 		Tavolo tavoloInserito = tavoloService.inserisciNuovo(tavoloInput.buildTavoloModel(), principal.getName());
 		return TavoloDTO.buildTavoloDTOFromModel(tavoloInserito, false);
+	}
+	
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void delete(@PathVariable(required = true) Long id, Principal principal) {
+		tavoloService.rimuovi(id, principal.getName());
 	}
 }
