2561fb59d0940485345b4b8f833affb47a71f889
MattiaLR95 <laroccamattia@virgilio.it>
2022-12-02 16:03:30 +0100

    Implementata la funzione di aggiornamento tavolo

--- src/main/java/it/prova/pokeronline/web/api/TavoloController.java
+++ src/main/java/it/prova/pokeronline/web/api/TavoloController.java
@@ -6,11 +6,13 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -59,5 +61,18 @@ public class TavoloController {
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void delete(@PathVariable(required = true) Long id, Principal principal) {
 		tavoloService.rimuovi(id, principal.getName());
+	}
+	
+	@PutMapping("/{id}")
+	public TavoloDTO update(@Valid @RequestBody TavoloDTO tavoloInput, @PathVariable(required = true) Long id,
+			Principal principal) {
+		Tavolo tavolo = tavoloService.caricaSingoloTavolo(id);
+
+		if (tavolo == null)
+			throw new TavoloNotFoundException("Tavolo not found con id: " + id);
+
+		tavoloInput.setId(id);
+		Tavolo tavoloAggiornato = tavoloService.aggiorna(tavoloInput.buildTavoloModel(), principal.getName());
+		return TavoloDTO.buildTavoloDTOFromModel(tavoloAggiornato, false);
 	}
 }
