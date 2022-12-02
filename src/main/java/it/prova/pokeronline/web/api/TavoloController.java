563ebad65a3e2d8d7fb530a7c1ea991786b34980
MattiaLR95 <laroccamattia@virgilio.it>
2022-12-02 16:02:00 +0100

    Implementata la funzione di aggiunta tavolo

--- src/main/java/it/prova/pokeronline/web/api/TavoloController.java
+++ src/main/java/it/prova/pokeronline/web/api/TavoloController.java
@@ -3,15 +3,20 @@ package it.prova.pokeronline.web.api;
 import java.security.Principal;
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import it.prova.pokeronline.dto.TavoloDTO;
 import it.prova.pokeronline.model.Tavolo;
 import it.prova.pokeronline.service.TavoloService;
+import it.prova.pokeronline.web.api.exception.IdNotNullForInsertException;
 import it.prova.pokeronline.web.api.exception.TavoloNotFoundException;
 
 @RestController
@@ -36,4 +41,14 @@ public class TavoloController {
 		return TavoloDTO.buildTavoloDTOFromModel(tavolo, false);
 	}
 	
+	@PostMapping
+	public TavoloDTO createNew(@Valid @RequestBody TavoloDTO tavoloInput, Principal principal) {
+		// se mi viene inviato un id jpa lo interpreta come update ed a me (producer)
+		// non sta bene
+		if (tavoloInput.getId() != null)
+			throw new IdNotNullForInsertException("Non è ammesso fornire un id per la creazione");
+
+		Tavolo tavoloInserito = tavoloService.inserisciNuovo(tavoloInput.buildTavoloModel(), principal.getName());
+		return TavoloDTO.buildTavoloDTOFromModel(tavoloInserito, false);
+	}
 }
